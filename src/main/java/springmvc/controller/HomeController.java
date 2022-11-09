3c54d0a285f881627545fff5bd43035e7b02a8fe
BRD\siddik.shaikh <siddik.shaikh@collabera.com>
2022-11-09 12:30:44 +0530

    data send from controller to view using Model Object

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,18 @@
 package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 @RequestMapping("/home")	
-public String home() {
+public String home(Model model) {
 	System.out.println("This is home URL");
+	model.addAttribute("name","Shaikh");
+	model.addAttribute("roll",1233);
+	model.addAttribute("isTrue",false);
 	return "index";
    }
 @RequestMapping("/about")
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -10,5 +10,14 @@
 <h1>This is Home page</h1>
 <h1>Called by Home controller</h1>
 <h1>url  /home </h1>
+<% String name= (String) request.getAttribute("name"); 
+Integer number=(Integer) request.getAttribute("roll");
+Boolean isTrue= (Boolean)request.getAttribute("isTrue");
+out.println(name);
+out.println("Roll number is "+number);
+
+%>
+<h1>Name is <%=name%></h1>
+<h1> isFalse <%=isTrue %></h1>
 </body>
 </html>
