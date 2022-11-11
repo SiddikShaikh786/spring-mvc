4d41334ca9084d759c30e1da2bf069622b95e64b
BRD\siddik.shaikh <siddik.shaikh@collabera.com>
2022-11-11 10:18:59 +0530

    handling form @ spring mvc controller using @RequestParam Annotation

--- src/main/java/springmvc/controller/ContactController.java
+++ src/main/java/springmvc/controller/ContactController.java
@@ -3,8 +3,10 @@ package springmvc.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ContactController {
 
@@ -15,9 +17,20 @@ public class ContactController {
 	}
 	
 	@RequestMapping(path ="/processform",method = RequestMethod.POST)
-	public String handlerForm(HttpServletRequest request) {
-	String name=	request.getParameter("userName");
-	System.out.println(name);
-		return "";
+	public String handlerForm(
+			@RequestParam("userName") String userName,
+			@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			Model model)
+	{
+		System.out.println("user name "+userName);
+		System.out.println("your Email "+email);
+		System.out.println("password "+password);
+		
+		model.addAttribute("name", userName);
+		model.addAttribute("email",email);
+		model.addAttribute("password",password);
+	
+		return "success";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@page isELIgnored="false" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Success</title>
+</head>
+<body>
+<h1>${name }</h1>
+<h1>${email }</h1>
+<h1>${password }</h1>
+</body>
+</html>
